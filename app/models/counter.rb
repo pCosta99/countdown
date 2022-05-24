2998c6bfac05bb9878f839d03cbd0b1dc4ddadff
Pedro Costa <costapedro.a1999@gmail.com>
2022-05-25 00:07:03 +0100

    validate deadline not in the past; presence of name

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Counter < ApplicationRecord
+  validate :not_past_deadline, on: :create
+  validate :name, :presence
+
+  def not_past_deadline
+    errors.add(:deadline, "Can't be in the past") if deadline < DateTime.now
+  end
 end
